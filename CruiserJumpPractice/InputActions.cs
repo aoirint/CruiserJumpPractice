e96d34994d4402129cd3c90e346b16413f093f23
aoirint <aoirint@gmail.com>
2025-11-29 22:00:57 +0900

    chore: update keymap comment

--- CruiserJumpPractice/InputActions.cs
+++ CruiserJumpPractice/InputActions.cs
@@ -8,15 +8,15 @@ namespace CruiserJumpPractice;
 
 class InputActions : LcInputActions
 {
-    // NOTE: JP109 @, US [
+    // Keymap: JP109 @, US [
     [InputAction(KeyboardControl.LeftBracket, Name = "Load Cruiser")]
     public InputAction? LoadCruiserKey { get; set; }
 
-    // NOTE: JP109 [, US ]
+    // Keymap: JP109 [, US ]
     [InputAction(KeyboardControl.RightBracket, Name = "Save Cruiser")]
     public InputAction? SaveCruiserKey { get; set; }
 
-    // NOTE: JP109 ], US \
+    // Keymap: JP109 ], US \
     [InputAction(KeyboardControl.Backslash, Name = "Toggle Magnet")]
     public InputAction? ToggleMagnetKey { get; set; }
 }
